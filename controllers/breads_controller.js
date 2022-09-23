332dc01cf9e8cb21dafd203b1aef61e939dd3834
E'laishah Ramirez <drz0224@gmail.com>
2022-09-22 19:18:40 -0700

    fixed issues with port

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,14 +4,13 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.render('index')
-// res.send(Bread)
+  res.send(Bread)
+})
+
+// SHOW
+breads.get('/:arrayIndex', (req, res) => {
+  res.send(Bread[req.params.arrayIndex])
 })
 
 
-
-// SHOW
-// breads.get('/:arrayIndex', (req, res) => {
-//   res.send(Bread[req.params.arrayIndex])
-// })
 module.exports = breads
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
-// MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
+// DEPENDENCIES
+const express =  require('express')
 
-
-const express = require('express')
-
-// CONFIGURATION
+//  CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
-// LISTEN
+  res.send('Welcome to an Awesome App about Breads!')
+})
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('breads', breadsController)
+
+// LISTEN 
 app.listen(PORT, () => {
-    console.log('listening on port', PORT);
+  console.log('listening on port',PORT)
 })
